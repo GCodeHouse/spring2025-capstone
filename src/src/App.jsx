84d86530d72c42c2c7303ab45212d4b75f5b456c
J-O-S-I-E <128793939+J-O-S-I-E@users.noreply.github.com>
2025-08-25 20:28:53 -0400

    Initial React Router Commit

--- src/src/App.jsx
+++ src/src/App.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import { useState } from 'react'
-import Timer from './components/Timer.jsx'
 import './App.css'
+import Home from './components/Home.jsx';
+import About from './components/About.jsx';
+
 
 function App() {
 
   return (
-    <>
-      <Timer duration={10}/>
-    </>
+
+    <Router>
+  
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+      </Switch>
+    </Router>
   )
 }
 
