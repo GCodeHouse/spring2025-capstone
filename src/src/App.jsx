3d58f5f2d406817b68a09a879c7b6a823a012865
elephino <colin.snyder@gmail.com>
2025-08-26 15:00:40 -0400

    Fix for BrowserRouter and empty About component

--- src/src/App.jsx
+++ src/src/App.jsx
@@ -1,26 +1,25 @@
 import React from 'react';
 import { Route, Routes, Link } from 'react-router-dom';
-import { useState } from 'react'
 import './App.css'
 import Home from './components/Home.jsx';
-import About from './components/About.jsx';
+{/* import About from './components/About.jsx'; */}
 
 
 function App() {
 
   return (
-<>
-    <div>
-      <nav className="nav">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About/>} />
-      </Routes>
-    </div>
-    </>
+    <>
+      <div>
+        <nav className="nav">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          {/* <Route path="/about" element={<About/>} /> */}
+        </Routes>
+      </div>
+    </> 
   );
 }
 
--- src/src/main.jsx
+++ src/src/main.jsx
@@ -1,13 +1,21 @@
 import { StrictMode } from 'react'
-import {BrowserRouter} from 'react-router-dom'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <BrowserRouter>
-    <App />
-    </BrowserRouter>
+   <RouterProvider router={router} />
   </StrictMode>,
 )
